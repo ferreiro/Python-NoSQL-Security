efcce4980eaf5564af0a22a2bc97d1753f7b3866
Jorge Ferreiro <me@jgferreiro.com>
2016-01-12 17:18:54 +0100

    new users_by_country_agg

--- assignment4/consultas_mr_agg.py
+++ assignment4/consultas_mr_agg.py
@@ -29,6 +29,7 @@ otros ni haberlo obtenido de una fuente externa.
 import pymongo
 import math
 
+from bson.son import SON
 from bson.code import Code
 from bottle import get, run, request, template
 from pymongo import MongoClient # install MongoClient
@@ -60,15 +61,22 @@ def users_by_country_mr():
 			""")
 
 	results = db.users.inline_map_reduce(mapper, reducer)
-	return template('table', results=results, count=len(results));
+	return template('table_map_reduce', results=results, count=len(results));
 
 # Aggregation Pipeline: usuarios en cada pais (orden descendente por numero
 # de usuarios).
 @get('/users_by_country_agg')
 def users_by_country_agg():
-    pass
-    
-    
+	
+	pipeline = [
+		{"$group": {"_id": "$country", "count": {"$sum": 1}}},
+		{"$sort": SON([("count", 1), ("_id", -1)])}
+	]
+	results= list(db.users.aggregate(pipeline))
+	return template('table_pipeline', results=results, count=len(results));
+	#db.command('aggregate', 'users', pipeline=pipeline, explain=True)
+
+
 # MapReduce: gasto total en cada pais.
 @get('/spending_by_country_mr')
 def spending_by_country_mr():
@@ -102,32 +110,32 @@ def spending_by_country_mr():
 			""")
 
 	results = db.users.inline_map_reduce(mapper, reducer)
-	return template('table', results=results, count=len(results));
+	return template('table_map_reduce', results=results, count=len(results));
 
 
 # Aggregation Pipeline: gasto total en cada pais (orden descendente por nombre
 # del pais).
 @get('/spending_by_country_agg')
 def spending_by_country_agg():
-    pass
+	pass
 
 
 # MapReduce: gasto total realizado por las mujeres que han realizdo EXACTAMENTE
 # 3 compras.
 @get('/spending_female_3_orders_mr')
 def spending_female_3_orders_mr():
-    pass
+	pass
 
 
 # Aggregation Pipeline: gasto total realizado por las mujeres que han realizdo 
 # EXACTAMENTE 3 compras.
 @get('/spending_female_3_orders_agg')
 def spending_female_3_orders_agg():
-    pass
-    
-    
+	pass
+	
+	
 ###############################################################################
 ###############################################################################
 
 if __name__ == "__main__":
-    run(host='localhost',port=8080,debug=True)
+	run(host='localhost',port=8080,debug=True)
