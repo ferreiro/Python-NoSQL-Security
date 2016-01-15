2dad2690bf07791dcd21612ecc9d2537d56d7b9b
luisfelipekai <luisfelipekai@gmail.com>
2016-01-15 22:07:22 +0100

    Spending Female 3 Orders MR DONE

--- assignment4/consultas_mr_agg.py
+++ assignment4/consultas_mr_agg.py
@@ -130,7 +130,32 @@ def spending_by_country_agg():
 # 3 compras.
 @get('/spending_female_3_orders_mr')
 def spending_female_3_orders_mr():
-	pass
+	mapper = Code("""
+			function spendingMap()
+			{
+				var total = 0;
+				var orders = this.orders;
+
+				if (orders) {
+					for (i=0; i < orders.length; i++) {
+						total += orders[i].total
+					}
+				}
+
+				emit("Total", { count: total });
+			}
+			""")
+	reducer = Code("""
+			function spendingReduce(key, values) {
+				var total = 0;
+				for (var i = 0; i < values.length; i++) {
+					total += values[i].count;
+				}
+				return { count: total }
+			}
+			""")
+	results = db.users.inline_map_reduce(mapper, reducer, query={"orders":{"$size":3}, "gender":"Female"})
+	return template('table_map_reduce', results=results, count=len(results));
 
 
 # Aggregation Pipeline: gasto total realizado por las mujeres que han realizdo 
@@ -138,7 +163,7 @@ def spending_female_3_orders_mr():
 @get('/spending_female_3_orders_agg')
 def spending_female_3_orders_agg():
 	pass
-	
+
 	
 ###############################################################################
 ###############################################################################
