68e1223b7e468eb6704f62aab40c63767cfbb6d4
Jorge Ferreiro <me@jgferreiro.com>
2016-01-17 03:17:58 +0100

    Tons of improvements on different functions + views

--- assignment4/consultas_mr_agg.py
+++ assignment4/consultas_mr_agg.py
@@ -7,62 +7,68 @@ Este código es fruto ÚNICAMENTE del trabajo de sus miembros. Declaramos no hab
 colaborado de ninguna manera con otros grupos, haber compartido el ćodigo con
 otros ni haberlo obtenido de una fuente externa.
 
-	Implicit Schema
-	+------------------------------------------------------+
-	| key                | types  | occurrences | percents |
-	| ------------------ | ------ | ----------- | -------- |
-	| _id                | String |           1 |    100.0 |
-	| country            | String |           1 |    100.0 |
-	| gender             | String |           1 |    100.0 |
-	| orders             | Array  |           1 |    100.0 |
-	| orders.XX.item     | String |           1 |    100.0 |
-	| orders.XX.quantity | Number |           1 |    100.0 |
-	| orders.XX.total    | Number |           1 |    100.0 |
-	+------------------------------------------------------+
+Implicit Schema
++------------------------------------------------------+
+| key                | types  | occurrences | percents |
+| ------------------ | ------ | ----------- | -------- |
+| _id                | String |           1 |    100.0 |
+| country            | String |           1 |    100.0 |
+| gender             | String |           1 |    100.0 |
+| orders             | Array  |           1 |    100.0 |
+| orders.XX.item     | String |           1 |    100.0 |
+| orders.XX.quantity | Number |           1 |    100.0 |
+| orders.XX.total    | Number |           1 |    100.0 |
++------------------------------------------------------+
 
-	orders has an array, where each order has (item, quantity and total)
+orders is an array with attributes (item, quantity and total)
+
 """
 
 # Importaciones
 import pymongo
 import math
 
+from bottle import get, run, template, route, static_file
+from pymongo import MongoClient 
 from bson.son import SON
 from bson.code import Code
-from bottle import get, run, request, template
-from pymongo import MongoClient # install MongoClient
-from pymongo import ReturnDocument
 
 client 	= MongoClient()
 db 		= client['giw']
+
+# Assets: in order to use images on the view
+@route('/<filename:re:.*\.(jpg|png|gif|ico)>')
+def images(filename):
+    return static_file(filename, root='views/')
 
 # MapReduce: usuarios en cada pais.
 @get('/users_by_country_mr')
 def users_by_country_mr():
 
 	mapper = Code("""
-			function countryMap() {
-				emit(this.country, { count: 1 });
-			}
-			""")
+		function countryMap() {
+			emit(this.country, { count: 1 });
+		}
+		""")
 
 	reducer = Code("""
-			function countryReduce(key, values) {
-				var total = 0;
+		function countryReduce(key, values) {
+			var total = 0;
 
-				for (var i = 0; i < values.length; i++) {
-					total += values[i].count;
-				}
-
-				return { count: Math.round(total) }
+			for (var i = 0; i < values.length; i++) {
+				total += Math.round(values[i].count);
 			}
-			""")
+
+			print(typeof(total))
+			return { count: NumberInt(total) }
+		}
+		""")
 
 	results = db.users.inline_map_reduce(mapper, reducer)
 	return template('table_map_reduce', results=results, count=len(results));
 
-# Aggregation Pipeline: usuarios en cada pais (orden descendente por numero
-# de usuarios).
+# Aggregation Pipeline: 
+# usuarios en cada pais (orden descendente por numero de usuarios).
 @get('/users_by_country_agg')
 def users_by_country_agg():
 	
@@ -70,6 +76,7 @@ def users_by_country_agg():
 		{"$group": {"_id": "$country", "count": {"$sum": 1}}},
 		{"$sort": SON([("count", -1), ("_id", -1)])}
 	]
+
 	results= list(db.users.aggregate(pipeline))
 	return template('table_pipeline', results=results, count=len(results));
 
@@ -78,12 +85,11 @@ def users_by_country_agg():
 def spending_by_country_mr():
 
 	mapper = Code("""
-			function countryMap() 
-			{
+			function mapper() {
 				var total = 0;
 				var orders = this.orders;
 
-				if (orders) {
+				if (orders) { // Don't access null orders
 					for (i=0; i < orders.length; i++) {
 						total += orders[i].total
 					}
@@ -94,7 +100,7 @@ def spending_by_country_mr():
 			""")
 
 	reducer = Code("""
-			function countryReduce(key, values) {
+			function reducer(key, values) {
 				var total = 0;
 
 				for (var i = 0; i < values.length; i++) {
@@ -108,17 +114,17 @@ def spending_by_country_mr():
 	results = db.users.inline_map_reduce(mapper, reducer)
 	return template('table_map_reduce', results=results, count=len(results));
 
-
-# Aggregation Pipeline: gasto total en cada pais (orden descendente por nombre
-# del pais).
+# Aggregation Pipeline: 
+# gasto total en cada pais (orden descendente por nombre del pais).
 @get('/spending_by_country_agg')
 def spending_by_country_agg():
 	
 	pipeline = [
 		{"$unwind": "$orders" },
 		{"$group": {"_id": "$country", "count": {"$sum": "$orders.total" }}},
-		{"$sort": SON([("count", 1), ("_id", -1)])}
+		{"$sort": SON([("_id", 1)])}
 	]
+
 	results= list(db.users.aggregate(pipeline))
 	return template('table_pipeline', results=results, count=len(results));
 
@@ -127,8 +133,10 @@ def spending_by_country_agg():
 @get('/spending_female_3_orders_mr')
 def spending_female_3_orders_mr():
 	
+	total = 0
+
 	mapper = Code("""
-			function countryMap() {	
+			function spendingMap() {	
 				if (this.gender == "Female") {
 					var total = 0;
 					var orders = this.orders;
@@ -139,48 +147,57 @@ def spending_female_3_orders_mr():
 								total += orders[i].total
 							}
 						}
+						emit(this.gender, total);
 					}
-					else {
-						print("No orders biach!")
-					}
-					
-					emit(this.gender, { count: total });
 				}
 			}
 			""")
 
 	reducer = Code("""
-			function countryReduce(key, values) {
+			function spendingReduce(key, values) {
 				var total = 0;
 
 				for (var i = 0; i < values.length; i++) {
-					total += values[i].count;
+					total += values[i]
 				}
 
-				return { count: total }
+				return total;
 			}
 			""")
 
 	results = db.users.inline_map_reduce(mapper, reducer)
-	return template('table_map_reduce', results=results, count=len(results));
+	if len(results) > 0: total= results[0]['value'] # set total with the result of the query
+	return template('totalfemale', total=total);
 
 # Aggregation Pipeline: gasto total realizado por las mujeres que han realizdo 
 # EXACTAMENTE 3 compras.
 @get('/spending_female_3_orders_agg')
 def spending_female_3_orders_agg():
+
+	total 	= 0
+
 	pipeline = [
-		{   
-			"$match" : { 
-				"gender" : "Female",
-				"orders": { "$size" : 3 }
-			}
+		{ "$match" : { 
+			"gender" : "Female", 
+			"orders": { "$size" : 3 }} 
 		},
 		{ "$unwind": "$orders" },
-		{ "$group": {"_id": "$gender", "count": {"$sum": "$orders.total" }}},
-		{ "$sort": SON([("count", 1), ("_id", -1)])}
+		{ "$group": {
+			"_id": "$gender", 
+			"count": {"$sum": "$orders.total" }}
+		},
+		{
+			"$project" : {
+				"_id": 0,
+				"count": 1
+			}
+		}
 	]
-	results= list(db.users.aggregate(pipeline))
-	return template('table_pipeline', results=results, count=len(results));
+	
+	results	= list(db.users.aggregate(pipeline))
+	if len(results) > 0: total= results[0]['count'] # set total with the result of the query
+
+	return template('totalfemale', total=total);
 
 	
 ###############################################################################
